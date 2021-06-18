5126ba677d85ba0c851d9cbccff687f26cf1fa52
Pyong-Hwa Baek <pyong-hwabaek@Pyong-Hwaui-MacBook-Pro.local>
2021-06-18 14:07:43 +0900

    Add Text

--- src/Components/Pages/Text/index.js
+++ src/Components/Pages/Text/index.js
@@ -9,6 +9,28 @@ function Text(props) {
     const pixiContainer = useRef()
 
     useEffect(() => {
+        const app = new PIXI.Application({
+            width: 512,         // default: 800
+            height: 512,        // default: 600
+            transparent: false, // default: false
+            antialias: true,   // default: false
+            resolution: 1,       // default: 1
+            backgroundColor: 0x787878
+        })
+        pixiContainer.current.appendChild(app.view)
+        let text = new PIXI.Text(
+            "This is PixiJS Text",
+            {
+                fontFamily: 'Arial',
+                fontSize: 32,
+                fill: 0xff1010,
+                align: 'center'
+            }
+        )
+        text.y = app.renderer.height / 2
+        text.alpha = 0.5
+        text.rotation = 5
+        app.stage.addChild(text)
 
     }, [])
 
