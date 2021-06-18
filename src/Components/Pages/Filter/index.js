f2c8249a7916a597b835ecd23561f60d1b5b3a1c
Pyong-Hwa Baek <pyong-hwabaek@Pyong-Hwaui-MacBook-Pro.local>
2021-06-18 12:30:21 +0900

    Add pixi-filters module to Filter

--- src/Components/Pages/Filter/index.js
+++ src/Components/Pages/Filter/index.js
@@ -1,6 +1,7 @@
 import withTitle from "../../Common/HOC/WithTitle"
 import {useEffect, useRef} from "react";
 import * as PIXI from 'pixi.js'
+import { DotFilter } from 'pixi-filters'
 import panda from '../../../Images/panda.jpeg'
 import monster from '../../../Images/monster.jpeg'
 import './index.scss'
@@ -19,6 +20,21 @@ function Filter(props) {
         })
         pixiContainer.current.appendChild(app.view)
 
+        const vertex = `
+            attribute vec2 aVertexPosition;
+            attribute vec2 aTextureCoord;
+            
+            uniform mat3 projectionMatrix;
+            
+            varying vec2 vTextureCoord;
+            
+            void main(void)
+            {
+                gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+                vTextureCoord = aTextureCoord;
+            }
+        `
+
         const fragment = `
             varying vec2 vTextureCoord;
             uniform sampler2D uSampler;
@@ -27,7 +43,8 @@ function Filter(props) {
                 gl_FragColor = texture2D(uSampler, vTextureCoord);
             }
         `
-        const myFilter = new PIXI.Filter(null, fragment)
+        const myFilter = new PIXI.Filter(vertex, fragment)
+        const dotFilter = new DotFilter()
 
         const texture = PIXI.Texture.from(monster);
         const img = new PIXI.Sprite(texture);
@@ -36,7 +53,7 @@ function Filter(props) {
         img.y = app.screen.height / 2;
         img.anchor.x = 0.5;
         img.anchor.y = 0.5;
-        img.filters = [myFilter];
+        img.filters = [dotFilter];
         app.stage.addChild(img);
 
     }, [])
