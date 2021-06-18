64d8c30db4adf2416e5a059cef3fb81e56c114c6
Pyong-Hwa Baek <pyong-hwabaek@Pyong-Hwaui-MacBook-Pro.local>
2021-06-18 13:55:59 +0900

    Update filter types

--- src/Components/Pages/Filter/index.js
+++ src/Components/Pages/Filter/index.js
@@ -1,7 +1,31 @@
 import withTitle from "../../Common/HOC/WithTitle"
 import {useEffect, useRef} from "react";
 import * as PIXI from 'pixi.js'
-import { DotFilter } from 'pixi-filters'
+import {
+    AdjustmentFilter,
+    AdvancedBloomFilter,
+    AsciiFilter,
+    BevelFilter,
+    BloomFilter,
+    ColorMapFilter,
+    ColorReplaceFilter,
+    ConvolutionFilter,
+    CrossHatchFilter,
+    CRTFilter,
+    DotFilter,
+    DropShadowFilter,
+    EmbossFilter,
+    GlitchFilter,
+    GlowFilter,
+    KawaseBlurFilter,
+    OldFilmFilter,
+    PixelateFilter,
+    ReflectionFilter,
+    RGBSplitFilter,
+    TiltShiftFilter,
+    TwistFilter,
+    ZoomBlurFilter,
+} from 'pixi-filters'
 import panda from '../../../Images/panda.jpeg'
 import monster from '../../../Images/monster.jpeg'
 import './index.scss'
@@ -44,17 +68,39 @@ function Filter(props) {
             }
         `
         const myFilter = new PIXI.Filter(vertex, fragment)
+        const adjustmentFilter = new AdjustmentFilter()
+        const advancedBloomFilter = new AdvancedBloomFilter()
+        const asciiFilter = new AsciiFilter()
+        const bevelFilter = new BevelFilter()
+        const bloomFilter = new BloomFilter()
+        const colorMapFilter = new ColorMapFilter()
+        const colorReplaceFilter = new ColorReplaceFilter()
+        const convolutionFilter = new ConvolutionFilter()
+        const crossHatchFilter = new CrossHatchFilter()
+        const crtFilter = new CRTFilter()
         const dotFilter = new DotFilter()
+        const dropShadowFilter = new DropShadowFilter()
+        const embossFilter = new EmbossFilter()
+        const glitchFilter = new GlitchFilter()
+        const glowFilter = new GlowFilter()
+        const kawaseBlurFilter = new KawaseBlurFilter()
+        const oldFilmFilter = new OldFilmFilter()
+        const pixelateFilter = new PixelateFilter()
+        const reflectionFilter = new ReflectionFilter()
+        const rgbSplitFilter = new RGBSplitFilter()
+        const tiltShiftFilter = new TiltShiftFilter()
+        const twistFilter = new TwistFilter()
+        const zoomBlurFilter = new ZoomBlurFilter()
 
-        const texture = PIXI.Texture.from(monster);
-        const img = new PIXI.Sprite(texture);
+        const texture = PIXI.Texture.from(monster)
+        const img = new PIXI.Sprite(texture)
 
-        img.x = app.screen.width / 2;
-        img.y = app.screen.height / 2;
-        img.anchor.x = 0.5;
-        img.anchor.y = 0.5;
-        img.filters = [dotFilter];
-        app.stage.addChild(img);
+        img.x = app.screen.width / 2
+        img.y = app.screen.height / 2
+        img.anchor.x = 0.5
+        img.anchor.y = 0.5
+        img.filters = [zoomBlurFilter]
+        app.stage.addChild(img)
 
     }, [])
 
