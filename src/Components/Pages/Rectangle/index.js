482429d6770e01a62e67b7947fc6e27e30401588
Pyong-Hwa Baek <pyong-hwabaek@Pyong-Hwaui-MacBook-Pro.local>
2021-06-18 14:46:50 +0900

    Add Text and Rectangle

--- src/Components/Pages/Rectangle/index.js
+++ src/Components/Pages/Rectangle/index.js
@@ -2,18 +2,44 @@ import withTitle from "../../Common/HOC/WithTitle"
 import {useEffect, useRef} from "react";
 import * as PIXI from 'pixi.js'
 import panda from '../../../Images/panda.jpeg'
-import monster from '../../../Images/monster.jpeg'
 import './index.scss'
 
 function Rectangle(props) {
     const pixiContainer = useRef()
     useEffect(() => {
+        const app = new PIXI.Application({
+            width: 512,         // default: 800
+            height: 512,        // default: 600
+            transparent: false, // default: false
+            antialias: true,   // default: false
+            resolution: 1,       // default: 1
+            backgroundColor: 0xFFFFFF
+        })
+        pixiContainer.current.appendChild(app.view)
+
+        app.loader.add("panda", panda)
+            .load((loader,resources) => {
+                const rect = new PIXI.Rectangle(100, 50, 256, 256)
+                const texture = new PIXI.Texture(resources.panda.texture, rect)
+                let sprite = new PIXI.Sprite(texture)
+                app.stage.addChild(sprite)
+
+                console.log(`rect.top: ${rect.top}`)
+                console.log(`rect.right: ${rect.right}`)
+                console.log(`rect.bottom: ${rect.bottom}`)
+                console.log(`rect.left: ${rect.left}`)
+                console.log(`rect.contains(x, y): ${rect.contains(110, 60)}`)
+
+            })
 
     }, [])
 
     return (
         <div className="rectangle-root-container">
             <h3>Rectangle</h3>
+            <p>
+                Please check Console.
+            </p>
             <div className="pixi-container" ref={pixiContainer}></div>
         </div>
     )
