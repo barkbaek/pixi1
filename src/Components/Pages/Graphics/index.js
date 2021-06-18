da97031dd38ddcdb945b3c448f8c6a6e949c15e8
Pyong-Hwa Baek <pyong-hwabaek@Pyong-Hwaui-MacBook-Pro.local>
2021-06-18 11:26:11 +0900

    Update PIXI.Graphics

--- src/Components/Pages/Graphics/index.js
+++ src/Components/Pages/Graphics/index.js
@@ -1,9 +1,70 @@
 import withTitle from "../../Common/HOC/WithTitle"
+import {useEffect, useRef} from "react";
+import * as PIXI from 'pixi.js'
+import panda from '../../../Images/panda.jpeg'
+import monster from '../../../Images/monster.jpeg'
+import './index.scss'
 
 function Graphics(props) {
+    const pixiContainer = useRef()
+
+    useEffect(() => {
+        const app = new PIXI.Application({
+            width: 512,         // default: 800
+            height: 512,        // default: 600
+            transparent: false, // default: false
+            antialias: true,   // default: false
+            resolution: 1,       // default: 1
+            backgroundColor: 0xFFFFFF, // default: 0x000000
+        })
+        pixiContainer.current.appendChild(app.view)
+        let graphic = new PIXI.Graphics()
+        graphic.x = app.renderer.width / 2
+        graphic.y = app.renderer.height / 2
+        app.stage.addChild(graphic)
+
+        graphic.beginFill(0xff0000)
+        graphic.arc(0, 0, 50, 0, Math.PI)
+        graphic.endFill()
+
+        graphic.beginFill(0x00ff00)
+        graphic.drawCircle(-190, -190, 60)
+        graphic.endFill()
+
+        graphic.beginFill(0x770077)
+        graphic.drawEllipse(-170, 0, 80, 50)
+        graphic.endFill()
+
+        graphic.beginFill(0x0000ff)
+        graphic.drawRect(-80, -240, 50, 100)
+        graphic.endFill()
+
+        graphic.beginFill(0xffff00)
+        graphic.drawStar(70, -170, 5, 80, 30)
+        graphic.endFill()
+
+        graphic.beginFill(0x00ffff)
+        graphic.drawShape(new PIXI.Circle(0, 0, 10))
+        graphic.endFill()
+
+        graphic.beginFill(0xff00ff)
+        graphic.drawPolygon([new PIXI.Point(100, 100), new PIXI.Point(100, 200), new PIXI.Point(200, 100)]);
+        graphic.endFill()
+
+        graphic.moveTo(-200, 100)
+        graphic.lineStyle(5, 0x00ff00)
+        graphic.lineTo(-200, 150)
+        graphic.lineTo(-150, 150)
+        graphic.bezierCurveTo(-150, 150, -200, 125, -200, 100)
+        graphic.moveTo(0, 0)
+        graphic.quadraticCurveTo(0, 200, -200, 100)
+
+    }, [])
+
     return (
         <div className="graphics-root-container">
-            Graphics
+            <h3>Graphics</h3>
+            <div className="pixi-container" ref={pixiContainer}></div>
         </div>
     )
 }
--- src/Components/Pages/Texture/index.js
+++ src/Components/Pages/Texture/index.js
@@ -1,9 +1,75 @@
 import withTitle from "../../Common/HOC/WithTitle"
+import {useEffect, useRef} from "react";
+import * as PIXI from 'pixi.js'
+import panda from '../../../Images/panda.jpeg'
+import monster from '../../../Images/monster.jpeg'
+import './index.scss'
 
 function Texture(props) {
+    const pixiContainer = useRef()
+
+    useEffect(() => {
+        const app = new PIXI.Application({
+            width: 512,         // default: 800
+            height: 512,        // default: 600
+            transparent: false, // default: false
+            antialias: true,   // default: false
+            resolution: 1,       // default: 1
+        })
+        pixiContainer.current.appendChild(app.view)
+
+        const whiteTexture = PIXI.Texture.WHITE
+        const texture = PIXI.Texture.fromImage(panda)
+        console.log(`baseTexture: `)
+        console.dir(texture.baseTexture)
+        console.log(`defaultAnchor: `)
+        console.dir(texture.defaultAnchor)
+        console.log(`frame: `)
+        console.dir(texture.frame)
+        console.log(`width: `)
+        console.log(texture.width)
+        console.log(`height: `)
+        console.log(texture.height)
+        console.log(`noFrame: `)
+        console.log(texture.noFrame)
+        console.log(`orig: `)
+        console.log(texture.orig)
+        texture.rotate = 4
+        console.log(`rotate: `)
+        console.log(texture.rotate)
+        console.log(`textureCacheIds: `)
+        console.log(texture.textureCacheIds)
+        console.log(`valid: `)
+        console.log(texture.valid)
+
+        let sprite1 = new PIXI.Sprite(whiteTexture)
+        sprite1.width = 384
+        sprite1.height = 384
+        let sprite2 = new PIXI.Sprite(texture)
+        sprite2.width = 100
+        sprite2.height = 100
+
+        const texture2 = texture.clone()
+        let sprite3 = new PIXI.Sprite(texture2)
+        sprite3.x = 200
+        sprite3.y = 200
+        sprite3.width = 100
+        sprite3.height = 100
+
+        app.stage.addChild(sprite1)
+        app.stage.addChild(sprite2)
+        app.stage.addChild(sprite3)
+
+        setTimeout(() => {
+            console.log(`texture2 is destroyed.`)
+            texture2.destroy()
+        }, 2000)
+    }, [])
+
     return (
         <div className="texture-root-container">
-            Texture
+            <h3>Texture</h3>
+            <div className="pixi-container" ref={pixiContainer}></div>
         </div>
     )
 }
